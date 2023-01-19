a3bc3dbf7644e5980e91d882315e03a7b0325591
AnnaStef <117094766+AnnaStef@users.noreply.github.com>
2023-01-20 01:31:28 +0530

    Update A1.py

--- A1.py
+++ A1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import pyplot
 from numpy import arange
 from sklearn.cluster import KMeans
 from scipy.optimize import curve_fit
